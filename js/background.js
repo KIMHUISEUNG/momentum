0276911ce41830047416b1d88196a23864836543
hatso <mmorpg3636@naver.com>
2022-01-07 17:24:17 +0900

    add class id bgImage

--- js/background.js
+++ js/background.js
@@ -3,6 +3,7 @@ const images = ["food.jpg", "room.jpg", "tea.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
+bgImage.classList.add("myBackground");
 
 bgImage.src = `img/${chosenImage}`;
 
